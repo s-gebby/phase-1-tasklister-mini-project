fe1649fd42dc34f6b310c238ab66724e7f56aad3
silas <silasgebhart12@gmail.com>
2024-03-09 19:53:11 -0600

    final commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+const form = document.getElementById('creat-task-form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  const taskDescription = document.getElementById('new-task-description').value;
+
+  const newTaskElement = document.createElement('li');
+  newTaskElement.textContent = taskDescription;
+
+  const taskList = document.getElementById('task-list');
+  taskList.appendChild(newTaskElement);
+
+  document.getElementById('new-task-description').value = '';
+});
